c0be124fc78395028c218958e998a0141ac35aea
blizzard190 <22256@ma-web.nl>
2017-12-07 11:51:15 +0100

    Merge
    Latest version of Camera

--- slice_project1/Assets/Scripts/Camera/Camera_change.cs
+++ slice_project1/Assets/Scripts/Camera/Camera_change.cs
@@ -2,24 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera_change : MonoBehaviour {
+public class Camera_change : MonoBehaviour
+{
     [SerializeField]
     public GameObject CameraPoint;
     public GameObject CameraPoint1;
 
-    private int Rotation = 57;
+    private const int Rotation = 57;
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player" && this.gameObject.transform.position.z > other.gameObject.transform.position.z)
         {
-            Camera.main.transform.position = CameraPoint.transform.position;
-            Camera.main.transform.rotation = Quaternion.Euler(Rotation, 0, 0);
+            MoveCamera(CameraPoint.transform.position, new Vector3(Rotation, 0, 0));
         }
         else if (other.gameObject.tag == "Player" && this.gameObject.transform.position.z < other.gameObject.transform.position.z)
         {
-            Camera.main.transform.position = CameraPoint1.transform.position;
-            Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0);
+            MoveCamera(CameraPoint1.transform.position, new Vector3(0, 0, 0));
         }
+    }
+
+    private void MoveCamera(Vector3 position, Vector3 rotation)
+    {
+        Camera.main.transform.position = position;
+        Camera.main.transform.rotation = Quaternion.Euler(rotation);
     }
 }
